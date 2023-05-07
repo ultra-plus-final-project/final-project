8c8b79e71f76673cdf265fe908ac4b295a067d84
HutakiHare <132063819+HutakiHare@users.noreply.github.com>
2023-05-07 21:32:18 +0800

    Add files via upload

---
+++ search_file_io.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+//#include "search_file_io.h"
+//input 未完善
+/*************************************test*******************************************/
+struct node{
+    int strt_month;
+    int strt_day;
+    bool whole_day;
+    bool alert;
+    int strt_time;
+    int end_time;
+    char activity[100];
+    char place[100];
+    char others[100];
+    struct node *next;
+};
+/**************************************test*********************************************/
+
+int get_content_from_file(struct node *ptr){
+    FILE *input_file;
+    char *content;
+    input_file=fopen("input.txt","r");
+    if (input_file == NULL) {
+        printf("Error opening input file!\n");
+        return 1;
+    }
+    while (fgets(content,sizeof(content),input_file)!=NULL) {
+        fscanf(input_file,"%c/%c %d %d %d %d %s %s %s",ptr->strt_month,ptr->strt_day,\
+        ptr->whole_day,ptr->alert,ptr->strt_time,ptr->end_time,ptr->activity,ptr->place,ptr->others);
+        ptr=ptr->next;
+    }
+    fclose(input_file);
+    return 1;
+}
+
+int write_content_on_file(struct node *ptr){
+    FILE *output_file;
+    output_file=fopen("output.txt","w");
+    if (output_file == NULL) {
+        printf("Error opening output file!\n");
+        return 1;
+    }
+    while(ptr!=NULL){
+        fprintf(output_file,"%d/%d %d %d %d %d %s %s %s\n",ptr->strt_month,\
+        ptr->strt_day,ptr->whole_day,ptr->alert,ptr->strt_time,ptr->end_time\
+        ,ptr->activity,ptr->place,ptr->others);
+        ptr=ptr->next;
+    }
+    fclose(output_file);
+    return 1;
+}
+
+void search(struct node *list,int time){
+    while(list!=NULL){
+        if(list->strt_time==time){
+            printf("%s at %d",list->activity,time);
+            break;
+        }else{
+            list=list->next;
+        }
+    }
+    if(list==NULL){
+        printf("You don't have activity at %d\n",time);
+    }
+}
+
+/****************************************test********************************************/
+struct node *add_to_list(struct node *list,int st_mon,int st_da,bool who,bool aler,int str_t,int end_t,char* ac,char *pl,char *ot){
+    struct node *tmp;
+    tmp=malloc(sizeof(struct node));
+    strcpy(tmp->activity,ac);
+    tmp->strt_month=st_mon;
+    tmp->strt_day=st_da;
+    tmp->whole_day=who;
+    tmp->alert=aler;
+    tmp->strt_time=str_t;
+    tmp->end_time=end_t;
+    strcpy(tmp->place,pl);
+    strcpy(tmp->others,ot);
+    tmp->next=list;
+    return tmp;
+}
+
+int main(){
+    struct node *ptr=NULL,*rec=NULL;
+    rec=malloc(sizeof(struct node));
+    ptr=add_to_list(ptr,5,5,1,1,9,20,"walk sheep","ROS","bring gress");
+    ptr=add_to_list(ptr,7,2,1,1,8,16,"take test","hell","bring pen");
+    write_content_on_file(ptr);
+    //printf("********\n");
+    get_content_from_file(rec);
+    //printf("********\n");
+    while(rec!=NULL){
+        printf("############\n");
+        printf("%d/%d %d %d %d %d %s %s %s\n",rec->strt_month,\
+        rec->strt_day,rec->whole_day,rec->alert,rec->strt_time,rec->end_time\
+        ,rec->activity,rec->place,rec->others);
+        rec=rec->next;
+    }
+    //printf("********\n");
+    return 0;
+}
+/****************************************test*************************************************/
