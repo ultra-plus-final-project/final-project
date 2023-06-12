350d5bedee337a688a283c38097baa06f8888d02
HutakiHare <132063819+HutakiHare@users.noreply.github.com>
2023-06-13 00:11:38 +0800

    Update print.c

--- print.c
+++ print.c
@@ -156,7 +156,9 @@ void print_calendar(int year,int month,int date){
 void print_week(int year,int month,int date){
     int day[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
     int leap[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
-    Event_date *cur = find_current_date(month,date); //find the current day
+    Event_date *tmp=malloc(sizeof(Event_date));
+    get_content_from_file(tmp);
+    Event_content *cur_event =find_day( tmp, month, date); //find the current day
     printf("%s         %4d         %2d         \n",WHITE,year,month);
     printf("  SUN  MON  TUE  WED  TUE  FRI  SAT \n");
     printf("%s+----+----+----+----+----+----+----+\n",BLUE);
