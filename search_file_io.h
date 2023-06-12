fa4739c78221fc14c045dae546ed5420fe537f1d
lululala0222 <119798443+lululala0222@users.noreply.github.com>
2023-06-12 23:33:32 +0800

    Delete search_file_io.h

--- search_file_io.h
+++
@@ -1,8 +0,0 @@
-#ifndef SEARCH_FILE_IO_H
-#define SEARCH_FILE_IO_H
-
-int get_content_from_file(struct node *ptr);
-int write_content_on_file(struct node *ptr);
-void search(struct node *list,int time);
-
-#endif
