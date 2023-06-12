1e5ac0fb565099d1ad369bfa27078c09f1710bfa
ccyunhsicc <132214950+ccyunhsicc@users.noreply.github.com>
2023-06-12 23:20:15 +0800

    print.h
    print.h

---
+++ print.h
@@ -0,0 +1,9 @@
+void print_event_date_list();
+
+void print_event_content_list(Event_content *list);
+
+void print_calendar(int year,int month,int date);
+
+void print_week(int year,int month,int date);
+
+void print_date(int year,int month,int date);
